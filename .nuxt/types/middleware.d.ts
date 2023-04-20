516b1fd6d31b10354bffed43edbc935a449b7045
Daniil Savitskii <danildaniel121@gmail.com>
2023-04-20 18:26:06 +0400

    delete folder

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/Users/sava/Documents/code/projects/learn/javascript/nuxt-fast-project/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
